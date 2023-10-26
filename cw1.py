1a25ea20a3269d4c2f987952c7e5bea9f25c1f48
Kamil Dolkowski <kamil.dolkowski@op.pl>
2023-10-26 08:57:29 +0200

    update

--- cw1.py
+++ cw1.py
@@ -4,10 +4,29 @@
 # else:
 #     print("x <= 10")
     
-wiek=int(input("Podaj wiek: "))
-if wiek>=18:
-    print("Dorosły")
-elif wiek>10:
-    print("Młodzież")
-else:
-    print("Dziecko")
+# wiek=int(input("Podaj wiek: "))
+# if wiek>=18:
+#     print("Dorosły")
+# elif wiek>10:
+#     print("Młodzież")
+# else:
+#     print("Dziecko")
+
+# warunek=True
+# if warunek:
+#     print("True")
+# else:
+#     print("False")
+
+# x=int(input("Podaj liczbe: "))
+# if x%2==0 :
+#     print("Liczba parzysta")
+# else:
+#     print("Liczba nieparzysta")
+
+# wiek=int(input("Podaj wiek: "))
+# czy=input("Czy masz prawo jazdy (Tak/Nie): ").lower()
+# if wiek>=18 and czy=="tak":
+#     print("Tak")
+# else:
+#     print("Nie")
