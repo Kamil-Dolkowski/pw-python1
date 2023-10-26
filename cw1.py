7276e7127c60242f645b2e1e5ab0871b752fd5fb
Kamil Dolkowski <kamil.dolkowski@op.pl>
2023-10-26 08:47:03 +0200

    update

--- cw1.py
+++ cw1.py
@@ -1,6 +1,13 @@
-x=int(input("Podaj liczbe: "))
-if x>10:
-    print("x > 10")
-else:
-    print("x <= 10")
+# x=int(input("Podaj liczbe: "))
+# if x>10:
+#     print("x > 10")
+# else:
+#     print("x <= 10")
     
+wiek=int(input("Podaj wiek: "))
+if wiek>=18:
+    print("Dorosły")
+elif wiek>10:
+    print("Młodzież")
+else:
+    print("Dziecko")
