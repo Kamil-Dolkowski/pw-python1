fb8c02360855ad406da72db20c37db9255c0722c
Kamil Dolkowski <kamil.dolkowski@op.pl>
2023-11-14 08:25:49 +0100

    lekcja 14.11.2023

---
+++ Py 14.11.2023/date.py
@@ -0,0 +1,9 @@
+# Instrukcje formatowania daty i czasu w Pythonie przy użyciu stfftime() i strptime() są bardzo elastyczne. Oto niektóre z najczęściej używanych dyrektyw formatowania:
+%Y: Rok z pełną liczbą cyfr, np. "2023"
+%m Miesiąc jako liczba z zerem wiodącym, np. "01" do "12"
+%d: Dzien miesiąca jako liczba z zerem wiodącym, np. "01" do "31"
+%H Godzina (zegar 24-godz) jako liczba z zerem wiodącym, np. "00" do "23"
+%I Godzina (zegar 12-godz) jako liczba z zerem wiodącym, np. "01" do "12"
+%M Minuta jako liczba z zerem wiodącym, np. "00" do "59"
+%S Sekunda jako liczba z zerem wiodącym, cp. "00" do "59"
+%f Mikrosekundy jako liczba z zerem wiodącym, np.  "000000" do "999999"
