47de15c1538898e8310a9649e75fe391cfd434f4
Kamil Dolkowski <kamil.dolkowski@op.pl>
2023-11-29 20:36:35 +0100

    praca w domu

--- phonebook-spr1.py
+++ phonebook-spr1.py
@@ -2,7 +2,7 @@ file_path = "phonebook.txt"
 phonebook = []
 
 
-# funkcja, która pobiera dane z pliku i wyświetla jako tablicę
+# Czyta dane z pliku i zwraca je w postaci listy.
 def read_phonebook():
     try:
         with open(file_path, "r") as file:
@@ -14,20 +14,24 @@ def read_phonebook():
                 phonebook.append(line1)
             print("Wczytano z pliku: \n")
             print(phonebook)
-            return phonebook
     except FileNotFoundError: 
         print("Brak pliku.")
-            
+    return phonebook
+
+#Zapisuje dane książki telefonicznej do pliku.
 def save_phonebook():
     with open(file_path, "w") as file:
         for i in range(0,len(phonebook)):
             file.write(f"{phonebook[i][0]} {phonebook[i][1]}\n")
         return phonebook
-
+    
+# Wyświetla aktualną zawartość książki telefonicznej.
 def display_phonebook():
     print(phonebook)
 
-def add_entry(name,phone_number: str):
+#Dodaje nowy wpis do książki telefonicznej z podaną nazwą i numerem telefonu.
+def add_entry(name: str,phone_number: str):
+    print(" ")
     # sprawdzenie czy numer jest liczbą i ma 9 cyfr
     if not phone_number.isdigit() or len(phone_number) != 9:
         print("Błędny numer telefonu.")
@@ -38,35 +42,49 @@ def add_entry(name,phone_number: str):
                 print("Już jest taki numer telefonu.")
                 return phonebook
         phonebook.append([name,phone_number])  
-        print(" ")
         print(f"Dodano: {name} {phone_number} do phonebook'a.")
-        return phonebook
+    return phonebook
 
-def modify_entry(old_phone_number,new_name,new_phone_number):
+# Usuwa wpis na podstawie numeru telefonu.
+def remove_entry(phone_number: str):
+    print(" ")
+    for i in phonebook:
+        if i[1] == phone_number:
+            id = phonebook.index(i)
+            del phonebook[id]
+            print(f"Usunięto telefon o numerze '{phone_number}' z phonebook'a.")
+            return phonebook
+    print("Brak telefonu do usunięcia.")
+    return phonebook
+
+# Modyfikuje istniejący wpis, pozwalając na zmianę nazwy i numeru telefonu.
+def modify_entry(old_phone_number: str,new_name: str,new_phone_number: str):
+    print(" ")
     # sprawdzenie czy numer jest liczbą i ma 9 cyfr
     if not new_phone_number.isdigit() or len(new_phone_number) != 9:
         print("Błędny numer telefonu.")
     else:
+    # sprawdzenie, czy nie ma już takiego numeru tel.
+        for i in phonebook:
+            if i[1] == new_phone_number:
+                print("Już jest taki numer telefonu.")
+                return phonebook 
+    # modyfikowanie starego numeru telefonu
         for i in phonebook:
             if i[1] == old_phone_number:
                 old_name = i[0]
                 i[1] = new_phone_number
                 i[0] = new_name
-                return old_name, phonebook
+                print(f"Zmieniono: {old_name} {old_phone_number} na: {new_name} {new_phone_number}.")
+                return phonebook
         print("Brak takiego numeru telefonu.")
-
-def remove_entry(phone_number):
-    for i in phonebook:
-        if i[1] == phone_number:
-            id = phonebook.index(i)
-            del phonebook[id]
-            return
-    return print("Brak telefonu do usunięcia.")
+    return phonebook
 
 
 
 
 
+# Program główny.
 while True:
     print(" ")
     command = input("Enter command: ")
@@ -85,16 +103,12 @@ while True:
         phonebook = add_entry(name,phone_number)
     elif command == '-r':
         phone_number = input("Enter phone number: ")
-        remove_entry(phone_number)
-        print("")
-        print(f"Usunięto telefon o numerze '{phone_number}' z phonebook'a.")
+        phonebook = remove_entry(phone_number)
     elif command == '-m':
         old_phone_number = input("Enter old phone number: ")
         new_phone_number = input("Enter new phone number: ")
         new_name = input("Enter new name: ")
-        print("")
-        old_name, phonebook = modify_entry(old_phone_number, new_name, new_phone_number)
-        print(f"Zmieniono: {old_name} {old_phone_number} na: {new_name} {new_phone_number}.")
+        phonebook = modify_entry(old_phone_number, new_name, new_phone_number)
     elif command == '-h':
         print("-l   read_phonebook()")
         print("-s   save_phonebook()")
@@ -104,17 +118,6 @@ while True:
         print("-m   modify_entry()")
         print("-h   help")
         print("-e   exit\n")
-        
     elif command == '-e':
         print("Zakończono pracę programu.\n")
         break
-
-
-# read_phonebook()
-# add_entry('weronika','110373333')
-# save_phonebook()
-# print(phonebook)
-# remove_entry('110373333')
-
-# phonebook = [['kamil','12345'],['wieslaw','50567']]
-# save_phonebook()
---
+++ test.py
@@ -0,0 +1,43 @@
+# t=['kamil']
+# def funkcja():
+#     t[0]='aa'
+#     return t
+# t = funkcja()
+# print(t)
+
+
+# t=['kamil']
+# def funkcja():
+#     try:
+#         a=1
+#         b=2
+#         return a/b
+#     except ZeroDivisionError:
+#         print('a')
+#     return 0
+
+
+
+# x = funkcja()
+# print(x)
+
+
+x=5
+if x==5:
+    print("1")
+elif x<4:
+    print("2")
+else:
+    print("3")
+
+
+# if x to nie nr tel:
+#   "Błędny numer"
+# elif x == t:
+#   "Już jest taki numer"
+# else:
+#   (działania)
+# return phonebook
+
+
+
