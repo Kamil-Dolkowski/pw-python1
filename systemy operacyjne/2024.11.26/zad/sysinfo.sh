3ebf3b2bee3b6d7ca1f5d5c5e61d5048d67d113e
Kamil-Dolkowski <kamil.dolkowski@op.pl>
2024-11-26 13:41:05 +0100

    Lekcja 26.11.2024

--- systemy operacyjne/2024.11.26/zad/sysinfo.sh
+++ systemy operacyjne/2024.11.26/zad/sysinfo.sh
@@ -1,66 +1,88 @@
 #!/bin/bash
 
 
-cpu() {
-    echo "a"
+print_cpu() {
+    echo -n "CPU: " 
+    cat /proc/cpuinfo | head -n 5 | tail -n 1 | cut -c 14-100
 }
 
-# brak procentów
-memory() {
+print_memory() {
     total=$(free -m | awk 'NR>1 {print $2}' | head -n 1)
     used=$(free -m | awk 'NR>1 {print $3}' | head -n 1)
 
-    procent=$(($((used))/$((total))*100))
+    procent=$((100*$used/$total)) 
 
     echo "Memory: $used / $total MiB ($procent% used)"
 }
 
-load() {
-    echo -n "Load: " && uptime | awk '{print $8, $9, $10}'
+print_load() {
+    echo -n "Load: " 
+    uptime | awk '{print $8, $9, $10}'
 }
 
-uptime() {
-    echo -n "Uptime: " && uptime | awk '{print $3}' | tr -d ',' | awk -F: '{print $1 " hour,", $2 " minutes"}'
+print_uptime() {
+    echo -n "Uptime: "
+    uptime | awk '{print $3}' | tr -d ',' | awk -F: '{print $1 " hour,", $2 " minutes"}'
 }
 
-kernel() {
+print_kernel() {
 echo "a"
 }
 
-gpu() {
+print_gpu() {
 echo "a"
 }
 
-user() {
+print_user() {
+echo -n "User: "
+whoami
+}
+
+print_shell() {
 echo "a"
 }
 
-shell() {
+print_processes() {
 echo "a"
 }
 
-processes() {
+print_threads() {
 echo "a"
 }
 
-threads() {
+print_ip() {
 echo "a"
 }
 
-ip() {
-echo "a"
+print_dns() {
+    echo -n "DNS: "
+    cat /etc/resolv.conf | grep 'nameserver' | cut -c 12-27
 }
 
-dns() {
-echo "a"
-}
-
-internet() {
+print_internet() {
     ping 8.8.8.8 -W 1 -c 1
 }
 
 
-memory
+if [ $1 ] ; then 
+    echo "ss"
+else
+    print_cpu
+    print_memory
+    print_load
+    print_uptime
+    print_kernel
+    print_gpu
+    print_user
+    print_shell
+    print_processes
+    print_threads
+    print_ip
+    print_dns
+    print_internet
+fi
+
+
 
 
 # root -> zwraca 1
