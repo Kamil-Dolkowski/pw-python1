75f78c81c34b75b409c873622c01d3b327b6084d
Kamil-Dolkowski <kamil.dolkowski@op.pl>
2024-12-05 00:14:07 +0100

    ?

--- WypożyczalniaAut-widoki.sql
+++ WypożyczalniaAut-widoki.sql
@@ -15,3 +15,30 @@ SELECT c.name, c.surname, COUNT(r.customer_id) AS reserved_cars
 FROM reservations r
 JOIN customers c ON (r.customer_id = c.customer_id)
 GROUP BY r.customer_id,c.name,c.surname;
+
+
+--nie działa
+CREATE OR REPLACE PROCEDURE find_cars_by_manufacturer(manufact_name varchar(20))
+LANGUAGE PLPGSQL
+AS $$
+BEGIN
+    EXECUTE
+    'SELECT registration_number,manufacturer,model,color,reservation_state 
+    FROM cars 
+    WHERE manufacturer = $1;' USING manufact_name;
+END;
+$$;
+
+--nie działa
+CREATE OR REPLACE FUNCTION find_cars_by_manufacturer(manufact_name varchar(20))
+returns setof cars
+AS
+$$
+SELECT registration_number,manufacturer,model,color,reservation_state 
+FROM cars 
+WHERE manufacturer = manufact_name;
+$$
+LANGUAGE SQL;
+
+
+--select * from find_cars_by_manufacturer('Skoda');
