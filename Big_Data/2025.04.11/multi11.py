db75332dc3057ddf892f9736f9ccdd7cce2571db
Kamil-Dolkowski <kamil.dolkowski@op.pl>
2025-04-11 16:22:35 +0200

    BD 11.04.2025

---
+++ Big_Data/2025.04.11/multi11.py
@@ -0,0 +1,37 @@
+import multiprocessing
+from multiprocessing import Lock
+import time
+
+def job(n, lock):
+    print(f"Worker number {n}: begin")
+
+    if n == 4:
+        print(f"Worker number {n}: wait on lock")
+        lock.acquire()
+        time.sleep(10)
+        lock.release()
+    else:
+        time.sleep(1)
+        print(f"Worker number {n}: wait on lock")
+        lock.acquire()
+        pass
+        lock.release()
+
+    print(f"Worker number {n}: end")
+
+def test():
+    lock = Lock()
+    workers = []
+
+    for i in range(24):
+        worker = multiprocessing.Process(target=job, args=(i, lock))
+        workers.append(worker)
+        worker.start()
+
+    for worker in workers:
+        worker.join()
+
+    print("!!! DONE !!!")
+
+if __name__ == "__main__":
+    test()
