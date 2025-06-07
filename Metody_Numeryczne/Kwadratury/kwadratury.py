864c0310fa6c29b4abe480d4d5c414d27a2d537d
Kamil Dolkowski <kamil.dolkowski@op.pl>
2025-06-07 19:37:06 +0200

    dodanie wykresów

--- Metody_Numeryczne/Kwadratury/kwadratury.py
+++ Metody_Numeryczne/Kwadratury/kwadratury.py
@@ -1,8 +1,11 @@
 import sympy as sp
+import matplotlib.pyplot as plt
+import numpy as np
 
 MAX_DEPTH = 5_000
 
-def rectangles_method(F, a, b, E):
+# Metody całkowania
+def left_rectangles_method(F, a, b, E):
     n = 1
     last_sum = 0
 
@@ -11,6 +14,52 @@ def rectangles_method(F, a, b, E):
         dx = (b-a)/n
 
         for i in range(n):
+            current_sum += F(a + dx*i)
+
+        current_sum *= dx
+
+        # print(f"{n}: {current_sum}")
+
+        if n>1 and abs(last_sum - current_sum) <= E:
+            break
+        else:
+            last_sum = current_sum
+            n += 1
+
+    return current_sum, n
+
+def middle_rectangles_method(F, a, b, E):
+    n = 1
+    last_sum = 0
+
+    while (n < MAX_DEPTH):
+        current_sum = 0
+        dx = (b-a)/n
+
+        for i in range(n):
+            current_sum += F(a + dx*i + 0.5*dx)
+
+        current_sum *= dx
+
+        # print(f"{n}: {current_sum}")
+
+        if n>1 and abs(last_sum - current_sum) <= E:
+            break
+        else:
+            last_sum = current_sum
+            n += 1
+
+    return current_sum, n
+
+def right_rectangles_method(F, a, b, E):
+    n = 1
+    last_sum = 0
+
+    while (n < MAX_DEPTH):
+        current_sum = 0
+        dx = (b-a)/n
+
+        for i in range(1, n+1):
             current_sum += F(a + dx*i)
 
         current_sum *= dx
@@ -74,11 +123,128 @@ def simpson_method(F, a, b, E):
 
     return current_sum, n
 
+# Wyświetlenie wyników i wykresów dla poszczególnych metod
+def print_result_rectangles_method(F, a, b, E, is_figure = True):
+    fig = plt.figure()
+    fig.suptitle("Metoda prostokątów")
+
+    axs = [
+        plt.subplot2grid((2, 4), (0, 0), colspan=2), 
+        plt.subplot2grid((2, 4), (0, 2), colspan=2),
+        plt.subplot2grid((2, 4), (1, 1), colspan=2)
+    ]
+
+    # Prostokąty lewe
+    result, n = left_rectangles_method(F, a, b, E)
+    print_result_info("prostokąty (lewe)", result, n)
+    
+    dx = (b-a)/n
+    x_rect = np.linspace(a, b-dx, n)
+    y_rect = F(x_rect)
+
+    x = np.linspace(a, b, 1000)
+    axs[0].plot(x, F(x))
+    axs[0].bar(x_rect, y_rect, width=dx, align="edge", alpha=0.5, color="orange", edgecolor="orange", linewidth=1)
+    axs[0].set_title("Prostokąty lewe")
+    axs[0].set_xlabel("x")
+    axs[0].set_ylabel("y")
+
+    # Prostokąty prawe
+    result, n = right_rectangles_method(F, a, b, E)
+    print_result_info("prostokąty (prawe)", result, n)
+
+    dx = (b-a)/n
+    x_rect = np.linspace(a, b-dx, n)
+    y_rect = F(x_rect + dx)
+
+    x = np.linspace(a, b, 1000)
+    axs[1].plot(x, F(x))
+    axs[1].bar(x_rect, y_rect, width=dx, align="edge", alpha=0.5, color="orange", edgecolor="orange", linewidth=1)
+    axs[1].set_title("Prostokąty prawe")
+    axs[1].set_xlabel("x")
+    axs[1].set_ylabel("y")
+
+    # Prostokąty środkowe
+    result, n = middle_rectangles_method(F, a, b, E)
+    print_result_info("prostokąty (środkowe)", result, n)
+
+    dx = (b-a)/n
+    x_rect = np.linspace(a, b-dx, n)
+    y_rect = F(x_rect + 0.5*dx)
+
+    x = np.linspace(a, b, 1000)
+    axs[2].plot(x, F(x))
+    axs[2].bar(x_rect, y_rect, width=dx, align="edge", alpha=0.5, color="orange", edgecolor="orange", linewidth=1)
+    axs[2].set_title("Prostokąty środkowe")
+    axs[2].set_xlabel("x")
+    axs[2].set_ylabel("y")
+
+    if is_figure == True:
+        plt.tight_layout()
+        plt.show()
+
+def print_result_trapeziums_method(F, a, b, E, is_figure = True):
+    result, n = trapeziums_method(F, a, b, E)
+    print_result_info("trapezy", result, n)
+
+    x_trap = np.linspace(a, b, n+1)
+    y_trap = F(x_trap)
+
+    for i in range(n):
+        plt.fill_between([x_trap[i], x_trap[i+1]], [y_trap[i], y_trap[i+1]], alpha=0.5, color="orange")
+
+    x = np.linspace(a, b, 1000)
+    plt.plot(x, F(x))
+    plt.title("Metoda trapezów")
+    plt.xlabel("x")
+    plt.ylabel("y")
+
+    if is_figure == True:
+        plt.show()
+
+def print_result_simpson_method(F, a, b, E, is_figure = True):
+    result, n = simpson_method(F, a, b, E)
+    print_result_info("parabole (metoda Simpsona)", result, n)
+
+    x_trap = np.linspace(a, b, n+1)
+    y_trap = F(x_trap)
+
+    for j in range(n//2):
+        i = 2*j
+
+        # Interpolacja wielomianowa Lagrange'a drugiego stopnia
+        x = sp.symbols('x')
+        lagrange_formula = y_trap[i] * ((x - x_trap[i+1]) * (x - x_trap[i+2])) / ((x_trap[i] - x_trap[i+1]) * (x_trap[i] - x_trap[i+2]))
+        lagrange_formula += y_trap[i+1] * ((x - x_trap[i]) * (x - x_trap[i+2])) / ((x_trap[i+1] - x_trap[i]) * (x_trap[i+1] - x_trap[i+2]))
+        lagrange_formula += y_trap[i+2] * ((x - x_trap[i]) * (x - x_trap[i+1])) / ((x_trap[i+2] - x_trap[i]) * (x_trap[i+2] - x_trap[i+1]))
+
+        F_lagrange = sp.lambdify(x, lagrange_formula)
+
+        x_segment = np.linspace(x_trap[i], x_trap[i+2], 1000)
+        y_segment = [F_lagrange(x) for x in x_segment]
+
+        plt.fill_between(x_segment, y_segment, alpha=0.5, color="orange")
+
+    x_values = np.linspace(a, b, 1000)
+    plt.plot(x_values, F(x_values))
+    plt.title("Metoda Simpsona (parabol)")
+    plt.xlabel("x")
+    plt.ylabel("y")
+
+    if is_figure == True:
+        plt.show()
+
+
 def print_result_info(method_type, result, n):
     print("\n======== ROZWIĄZANIE ========")
     print(f"Metoda: {method_type}")
     print(f"Wynik: {result}")
-    if n < MAX_DEPTH:
+    if method_type == "parabole (metoda Simpsona)":
+        if n < MAX_DEPTH:
+            print(f"Ilość podziałów (k=0.5*n): {int(0.5*n)}")
+        else:
+            print(f"Ilość podziałów (k=0.5*n): {int(0.5*n)} [osiągnięto maksymalną wartość podziałów!]")
+    elif n < MAX_DEPTH:
         print(f"Ilość podziałów (n): {n}")
     else:
         print(f"Ilość podziałów (n): {n} [osiągnięto maksymalną wartość podziałów!]")
@@ -122,6 +288,12 @@ def main():
     if E < 0:
         print("\nBłąd: 'E' nie może być mniejsze od 0")
         return
+    
+    choice = input("\nCzy chcesz wykresy? [t/n]: ")
+    if choice in ["T", "t"]:
+        is_figure = True
+    else:
+        is_figure = False
 
     print("\nPodaj metodę (p/t/s/w):")
     print("p - prostokąty")
@@ -131,26 +303,20 @@ def main():
     method = input("Wybór: ")
 
     if method == "p":
-        result, n = rectangles_method(F, a, b, E)
-        print_result_info("prostokąty (lewe)", result, n)
+        print_result_rectangles_method(F, a, b, E, is_figure)
 
     elif method == "t":
-        result, n = trapeziums_method(F, a, b, E)
-        print_result_info("trapezy", result, n)
+        print_result_trapeziums_method(F, a, b, E, is_figure)
 
     elif method == "s":
-        result, n = simpson_method(F, a, b, E)
-        print_result_info("parabole (metoda Simpsona)", result, n)
+        print_result_simpson_method(F, a, b, E, is_figure)
         
     elif method == "w":
-        result, n = rectangles_method(F, a, b, E)
-        print_result_info("prostokąty (lewe)", result, n)
+        print_result_rectangles_method(F, a, b, E, is_figure)
 
-        result, n = trapeziums_method(F, a, b, E)
-        print_result_info("trapezy", result, n)
+        print_result_trapeziums_method(F, a, b, E, is_figure)
 
-        result, n = simpson_method(F, a, b, E)
-        print_result_info("parabole (metoda Simpsona)", result, n)
+        print_result_simpson_method(F, a, b, E, is_figure)
     else:
         print("\nBłąd: Niepoprawna wartość.")
         return
